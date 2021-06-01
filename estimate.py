9695f124f1f24a1e96d9761bb82276a39905bad3
Shibiliya Ismail <50785323+duaboola@users.noreply.github.com>
2021-06-01 23:45:43 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -19,8 +19,8 @@ def monte_carlo(n):
     p = [0,0]
     c = 0
     for i in range(n):
-        x = random.random(0.0,1.0)
-        y = random.random(0.0,1.0)
+        x = random.random(0,1)
+        y = random.random(0,1)
         q = [x,y]
         d = math.dist(p,q)
         if (d<1):
